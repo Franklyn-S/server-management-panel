287848962e764ea20718e6cbb679f9d66badabf1
Franklyn Seabra Rogerio Bezerra <franklyn.bezerra@serpro.gov.br>
2026-05-13 14:27:01 -0300

    Adiciona novos componentes e estilos para gerenciamento de tickets e controle de entrada

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
+})
+export class ControlComponent {
+  label = input.required<string>();
+  for = input.required<string>();
+  private el = inject(ElementRef);
+  onClick() {
+    console.log(`Control for ${this.for()} clicked!`);
+    console.log('Element:', this.el);
+  }
+}
